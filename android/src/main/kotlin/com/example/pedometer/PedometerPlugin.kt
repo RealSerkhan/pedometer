81c6f59cfbd5619aa181215e3bf16f6ddb990e29
Real Sarkhan <rserxanbeyli@creativeadvtech.com>
2022-04-10 14:10:25 +0400

    plugin has changed

--- android/src/main/kotlin/com/example/pedometer/PedometerPlugin.kt
+++ android/src/main/kotlin/com/example/pedometer/PedometerPlugin.kt
@@ -1,34 +1,54 @@
-package com.example.pedometer
-
+import android.Manifest
+import android.app.Activity
+import androidx.annotation.NonNull
 import android.hardware.Sensor
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.annotation.NonNull
-import io.flutter.embedding.engine.plugins.FlutterPlugin
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import com.example.pedometer.SensorStreamHandler
+
 import io.flutter.plugin.common.EventChannel
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import android.content.pm.PackageManager
 /** PedometerPlugin */
 class PedometerPlugin : FlutterPlugin {
     private lateinit var stepDetectionChannel: EventChannel
     private lateinit var stepCountChannel: EventChannel
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        /// Create channels
-        stepDetectionChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_detection")
-        stepCountChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_count")
-
-        /// Create handlers
-        val stepDetectionHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_DETECTOR)
-        val stepCountHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_COUNTER)
-
-        /// Set handlers
-        stepDetectionChannel.setStreamHandler(stepDetectionHandler)
-        stepCountChannel.setStreamHandler(stepCountHandler)
+       val pm: PackageManager = flutterPluginBinding.applicationContext.packageManager
+    if (pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
+   print(message = "is avaliable")
     }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        stepDetectionChannel.setStreamHandler(null)
-        stepCountChannel.setStreamHandler(null)
+      else{
+          print("not avaliable")
     }
+    /// Create channels
+    stepDetectionChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_detection")
+    stepCountChannel = EventChannel(flutterPluginBinding.binaryMessenger, "step_count")
+
+    /// Create handlers
+    val stepDetectionHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_DETECTOR)
+    val stepCountHandler = SensorStreamHandler(flutterPluginBinding, Sensor.TYPE_STEP_COUNTER)
+
+    /// Set handlers
+    stepDetectionChannel.setStreamHandler(stepDetectionHandler)
+    stepCountChannel.setStreamHandler(stepCountHandler)
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    stepDetectionChannel.setStreamHandler(null)
+    stepCountChannel.setStreamHandler(null)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    TODO("Not yet implemented")
+  }
 
 }
--- android/src/main/kotlin/com/example/pedometer/SensorStreamHandler.kt
+++ android/src/main/kotlin/com/example/pedometer/SensorStreamHandler.kt
@@ -2,6 +2,7 @@ package com.example.pedometer
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Looper
@@ -11,6 +12,10 @@ import io.flutter.plugin.common.EventChannel
 import android.os.Handler
 
 class SensorStreamHandler() : EventChannel.StreamHandler {
+
+    var stepCount :Int=0
+    var MagnitidePrevious :Double= 0.0
+
 
     private var sensorEventListener: SensorEventListener? = null
     private var sensorManager: SensorManager? = null
@@ -28,9 +33,15 @@ class SensorStreamHandler() : EventChannel.StreamHandler {
     }
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        if(sensor==null){
+            sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            sensorEventListener = accelemeterSensorEventListener(events!!);
+            sensorManager!!.registerListener(sensorEventListener,
+                sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
         if (sensor == null) {
             events!!.error("1", "$sensorName not available",
-                    "$sensorName is not available on this device");
+                    "$sensorName is not available on this device")
         } else {
             sensorEventListener = sensorEventListener(events!!);
             sensorManager!!.registerListener(sensorEventListener,
@@ -41,5 +52,28 @@ class SensorStreamHandler() : EventChannel.StreamHandler {
     override fun onCancel(arguments: Any?) {
         sensorManager!!.unregisterListener(sensorEventListener);
     }
+    fun accelemeterSensorEventListener(events: EventChannel.EventSink): SensorEventListener? {
+        return object : SensorEventListener {
+
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+            override fun onSensorChanged(event: SensorEvent) {
+                var prevStepCount: Int =stepCount
+                val x_acceleration: Float = event.values[0]
+                val y_acceleration: Float = event.values[1]
+                val z_acceleration: Float = event.values[2]
+                var Magnitude: Double= Math.sqrt((x_acceleration*x_acceleration+y_acceleration*y_acceleration+z_acceleration*z_acceleration).toDouble());
+                var MagnitudeDelta:Double=Magnitude-MagnitidePrevious
+                MagnitidePrevious=Magnitude;
+                if(MagnitudeDelta>6){
+                    stepCount++
+                }
+//                if(stepCount>prevStepCount){
+                    events.success(stepCount)
+//                }
+            }
+        }
+    }
+
 
 }
